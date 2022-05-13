6ee4ac9e348c9e6f5ec1bd9aca0c7738b1c8d559
Miroslav Taleiko <mtaleiko@gmail.com>
2022-05-13 17:31:16 +0300

    Add mongo test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp")
     .then(() => console.log("Mongo: Connection opened"))
@@ -12,6 +13,12 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My Camp", description: "Cheap camping" });
+    await camp.save();
+    res.send(camp);
+});
 
 app.listen(3000, () => console.log("Started on port 3000..."));
